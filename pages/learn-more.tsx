c0b01c7ac8f859593efbb459a311b1555b0f5c17
angel112 <angel.lakra@chainlabs.in>
2022-09-13 19:29:10 +0530

    added placeholder

--- pages/learn-more.tsx
+++ pages/learn-more.tsx
@@ -5,13 +5,14 @@ import Image from "next/image";
 
 const LearnMore = () => {
   const [image, setImage] = React.useState("");
+  const [placeholder, setPlaceholder] = React.useState("");
 
   useEffect(() => {
-    const img = `https://ik.imagekit.io/chainlabs/Schmint/gifs/${_.random(
-      1,
-      4
-    )}.gif`;
+    const randomNumber = (_.random(0, 499) % 3) + 1;
+    const img = `https://ik.imagekit.io/chainlabs/Schmint/gifs/${randomNumber}.gif`;
+    const placeholder = `https://ik.imagekit.io/chainlabs/Schmint/placeholders/${randomNumber}.jpeg`;
     setImage(img);
+    setPlaceholder(placeholder);
   }, []);
 
   return (
@@ -22,7 +23,14 @@ const LearnMore = () => {
         height="50.67rem"
       >
         {image && (
-          <Image src={image} alt="gif" layout="fill" objectFit="contain" />
+          <Image
+            src={image}
+            alt="gif"
+            layout="fill"
+            objectFit="contain"
+            placeholder="blur"
+            blurDataURL={placeholder}
+          />
         )}
       </Box>
     </Box>
