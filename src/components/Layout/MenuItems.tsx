a72eeafe36a8d55afc27abad054fd3d312da2ea7
angel112 <angel.lakra@chainlabs.in>
2022-09-26 22:58:35 +0530

    disable option in menu items

--- src/components/Layout/MenuItems.tsx
+++ src/components/Layout/MenuItems.tsx
@@ -13,17 +13,15 @@ const MenuItems = ({ userHasScheduler }) => {
 			<MenuItem Icon={Binoculars} text="Explore" route="/explore" />
 			<Box mt="mm" />
 			<MenuItem Icon={StarFour} text="My Schmints" route="/my-schmints" />
-			<If
-				condition={user.exists && userHasScheduler}
-				then={
-					<React.Fragment>
-						<Box mt="mm" />
-						<MenuItem Icon={SketchLogo} text="My Assets" route="/my-assets" />
-						<Box mt="mm" />
-						<MenuItem Icon={Wallet} text="Wallet" route="/wallet" />
-					</React.Fragment>
-				}
+			<Box mt="mm" />
+			<MenuItem
+				Icon={SketchLogo}
+				text="My Assets"
+				route="/my-assets"
+				disabled={!user.exists || !userHasScheduler}
 			/>
+			<Box mt="mm" />
+			<MenuItem Icon={Wallet} text="Wallet" route="/wallet" disabled={!user.exists || !userHasScheduler} />
 		</Box>
 	);
 };
