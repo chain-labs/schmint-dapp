f36ebd84475159858d84901d1da1f9504a6688d9
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-11-06 02:28:10 +0530

    responsive done

--- src/containers/Landing/constants.ts
+++ src/containers/Landing/constants.ts
@@ -1,22 +1,46 @@
 export const FAQs = [
 	{
 		q: 'What is schmint?',
-		a: 'Pellentesque nulla mi mattis non suspendisse. Mi, urna, pharetra in varius arcu, aliquet mi, mattis praesent. Vulputate justo consectetur elit vestibulum ante. In in egestas nunc egestas. Ridiculus leo orci in duis justo diam viverra sit lectus.',
+		a: 'Schmint is an automation platform that allows you (NFT collectors) to pre-order NFT mints and relax. It gives you an unfair advantage over others.',
 	},
 	{
-		q: 'What is schmint?',
-		a: 'Pellentesque nulla mi mattis non suspendisse. Mi, urna, pharetra in varius arcu, aliquet mi, mattis praesent. Vulputate justo consectetur elit vestibulum ante. In in egestas nunc egestas. Ridiculus leo orci in duis justo diam viverra sit lectus.',
+		q: 'What does Schmint means?',
+		a: 'Schedule + Mint = Schmint. Schmint is a mint scheduled for future.',
 	},
 	{
-		q: 'What is schmint?',
-		a: 'Pellentesque nulla mi mattis non suspendisse. Mi, urna, pharetra in varius arcu, aliquet mi, mattis praesent. Vulputate justo consectetur elit vestibulum ante. In in egestas nunc egestas. Ridiculus leo orci in duis justo diam viverra sit lectus.',
+		q: 'How to pronounce Schmint?',
+		a: '/shh-mint/',
 	},
 	{
-		q: 'What is schmint?',
-		a: 'Pellentesque nulla mi mattis non suspendisse. Mi, urna, pharetra in varius arcu, aliquet mi, mattis praesent. Vulputate justo consectetur elit vestibulum ante. In in egestas nunc egestas. Ridiculus leo orci in duis justo diam viverra sit lectus.',
+		q: 'Who is a Schminter?',
+		a: 'Schminters are deges who make use of Schmint to gain an unfair advantage against others.',
 	},
 	{
-		q: 'What is schmint?',
-		a: 'Pellentesque nulla mi mattis non suspendisse. Mi, urna, pharetra in varius arcu, aliquet mi, mattis praesent. Vulputate justo consectetur elit vestibulum ante. In in egestas nunc egestas. Ridiculus leo orci in duis justo diam viverra sit lectus.',
+		q: 'Can I use schmint to apply for allow-list?',
+		a: 'Schmint is not an app for creating or applying for allow-lists. Schmint is an automation tool for NFT minting and trading.',
+	},
+	{
+		q: 'Can I use Schmint to automate trading on Opensea or any other secondary marketplace?',
+		a: 'Schmint will offer functionality to automate trading on secondary markets. Soon, you will be able to define your own trading conditions. Before trading, you may define the conditions based on the price, volume, and activity. Schmint to NFTs is analogous to Limit Orders in the stock market.',
+	},
+	{
+		q: 'How does Schmint know when a collection goes live?',
+		a: 'Schmint makes use of Gelato, an automated system comprised of distributed nodes that monitors the collection/project’s status. The execution begins at the instance when the collection/project becomes online.',
+	},
+	{
+		q: 'Can I create a Schmint for private mints or allow-listed mints?',
+		a: 'Schminting is currently only supported for public mints. We also intend to assist private or allow-listed mints.',
+	},
+	{
+		q: 'How does Schmint ensure security?',
+		a: 'Schmint creates a new Gnosis Safe for you, which is owned exclusively by you. All the assets and funds are stored in Gnosis Safe. Gnosis Safe receives notification whenever a collection or project is live and mints NFT on your behalf. Schmint will never store your funds or assets.',
+	},
+	{
+		q: 'How are new projects listed on Schmint?',
+		a: 'Currently, Simplr’s team curate the projects that are listed on the platform. We list projects based on Simplr’s community input and when a project applies to be listed on Schmint using this form (https://form.jotform.com/222922224502041). Schmint will open gates in the future so that projects can be registered quickly and automatically.',
+	},
+	{
+		q: 'Is Schmint similar to Premint?',
+		a: 'Schmint is quite distinct from Premint, though.',
 	},
 ];
