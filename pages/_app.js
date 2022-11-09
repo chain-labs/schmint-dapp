fe5232602a38517dbe885dabdb91de658b2820cc
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-11-09 19:43:57 +0530

    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -103,21 +103,20 @@ const MyApp = ({ Component, pageProps }) => {
 				}
 				
 				`}</script>
-				<If
+				{/* <If
 					condition={process.env.NODE_ENV === 'production' && hostname === 'schmint.simplrhq.com'}
-					then={
-						<script
-							type="text/javascript"
-							dangerouslySetInnerHTML={{
-								__html: `(function(c,l,a,r,i,t,y){
-							c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-							t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-							y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-						})(window, document, "clarity", "script", "dn44p6y6mg");`,
-							}}
-						></script>
-					}
-				/>
+					then={ */}
+				<script type="text/javascript">
+					{`{
+								(function(c,l,a,r,i,t,y){
+									c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+									t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+									y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+								})(window, document, "clarity", "script", "dn44p6y6mg");
+							}`}
+				</script>
+				{/* }
+				/> */}
 			</Head>
 			<ThemeProvider theme={theme}>
 				<Wagmi>
